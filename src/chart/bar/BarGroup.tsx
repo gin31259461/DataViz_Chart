7a4553a48af6dc49cfac833c5ae6bd6899ed0109
Abner <qw0207060413@gmail.com>
2024-01-20 18:04:09 +0800

    fix bar group chart position offset

--- src/chart/bar/BarGroup.tsx
+++ src/chart/bar/BarGroup.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import * as d3 from 'd3';
-import PropTypes from 'prop-types';
-import { ChartStyle, MapDataProps } from '../style';
-import { RemoveChart } from '@/components/chart';
-import { getElementHeight, getElementWidth } from '@/utils/dom';
-import { groupData } from '@/utils/data';
-import { easeExpInOut, easeExpOut, NumberValue } from 'd3';
-import { createTooltip } from '@/components/tooltip';
-import { createLegend } from '@/components/legend';
-import { createSVG } from '@/components/svg';
-import { createXAxis, createYAxis } from '@/components/axis';
-import { createTitle } from '@/components/text';
+import { createXAxis, createYAxis } from "@/components/axis";
+import { RemoveChart } from "@/components/chart";
+import { createLegend } from "@/components/legend";
+import { createSVG } from "@/components/svg";
+import { createTitle } from "@/components/text";
+import { createTooltip } from "@/components/tooltip";
+import { groupData } from "@/utils/data";
+import { getElementHeight, getElementWidth } from "@/utils/dom";
+import * as d3 from "d3";
+import { NumberValue, easeExpInOut, easeExpOut } from "d3";
+import PropTypes from "prop-types";
+import React from "react";
+import { ChartStyle, MapDataProps } from "../style";
 
 export default function BarGroup(props: ChartStyle) {
 	const svgRef = React.useRef<SVGSVGElement>(null);
@@ -24,18 +24,33 @@ export default function BarGroup(props: ChartStyle) {
 		handleLoad();
 	}, [props]);
 
-	return <svg width={'100%'} ref={svgRef} />;
+	return <svg width={"100%"} ref={svgRef} />;
 }
 
-function CreateBarGroup(element: React.RefObject<SVGElement>, props: ChartStyle) {
-	let { data, mapper, base, margin, xAxis, yAxis, tooltip, animation, legend, font, fill } = props;
+function CreateBarGroup(
+	element: React.RefObject<SVGElement>,
+	props: ChartStyle
+) {
+	let {
+		data,
+		mapper,
+		base,
+		margin,
+		xAxis,
+		yAxis,
+		tooltip,
+		animation,
+		legend,
+		font,
+		fill,
+	} = props;
 
 	if (data.length === 0) return;
 
 	const x = d3.map<object, string>(data, mapper.getX);
-	let rowKeys: string[] = [];
-	mapper.keys.forEach((key) => rowKeys.push(key));
-	const newData = groupData(rowKeys, data, x);
+	const group = d3.map<object, object>(data, mapper.getGroup);
+	const keys = Object.keys(group[0]);
+	const groupedData = groupData(keys, group, x);
 
 	if (base.width === undefined) base.width = getElementWidth(element);
 	if (base.height === undefined) base.height = getElementHeight(element);
@@ -44,18 +59,26 @@ function CreateBarGroup(element: React.RefObject<SVGElement>, props: ChartStyle)
 
 	xAxis.range = [margin.left, base.width - margin.right];
 	yAxis.range = [base.height - margin.bottom, margin.top];
-	if (xAxis.domain === undefined) xAxis.domain = x.filter((d) => d !== '');
+	if (xAxis.domain === undefined) xAxis.domain = x.filter((d) => d !== "");
 	if (yAxis.domain === undefined) {
-		const domain2 = d3.max(newData, (obj) => d3.max(obj.value, (obj) => obj.y * 1.1));
+		const domain2 = d3.max(groupedData, (obj) =>
+			d3.max(obj.value, (obj) => obj.y * 1.1)
+		);
 		yAxis.domain = [0, domain2 !== undefined ? domain2 : 0];
 	}
 	xAxis.domain = new d3.InternSet<string>(xAxis.domain as Iterable<string>);
 
-	const xScale = d3.scaleBand<string>(xAxis.domain as Iterable<string>, xAxis.range).padding(xAxis.padding);
-	const yScale = d3.scaleLinear(yAxis.domain as Iterable<NumberValue>, yAxis.range);
+	const xScale = d3
+		.scaleBand<string>(xAxis.domain as Iterable<string>, xAxis.range)
+		.padding(xAxis.padding);
+	const yScale = d3.scaleLinear(
+		yAxis.domain as Iterable<NumberValue>,
+		yAxis.range
+	);
 
-	if (base.color === undefined) base.color = d3.schemeAccent as Iterable<string>;
-	const colorScale = d3.scaleOrdinal(rowKeys, base.color);
+	if (base.color === undefined)
+		base.color = d3.schemeAccent as Iterable<string>;
+	const colorScale = d3.scaleOrdinal(keys, base.color);
 
 	const xAxisGenerator = d3
 		.axisBottom(xScale)
@@ -68,10 +91,11 @@ function CreateBarGroup(element: React.RefObject<SVGElement>, props: ChartStyle)
 			return `Group : ${d.key}\nx : ${d.x}\ny : ${d.y}`;
 		};
 
-	const barGap = 3;
-	const barWidth = xScale.bandwidth() / 3 - (rowKeys.length - 1) * barGap;
+	const barGap = 10;
+	const barWidth = xScale.bandwidth() / 20;
 
-	if (font.size === undefined) font.size = Math.min(base.width, base.height) / barWidth + 'px';
+	if (font.size === undefined)
+		font.size = Math.min(base.width, base.height) / barWidth + "px";
 
 	if (xAxis.enabled) {
 		createXAxis(
@@ -79,7 +103,7 @@ function CreateBarGroup(element: React.RefObject<SVGElement>, props: ChartStyle)
 			xAxisGenerator,
 			props,
 			false,
-			barWidth * x.length * rowKeys.length + barGap * x.length * (rowKeys.length - 1),
+			barWidth * x.length * keys.length + barGap * x.length * (keys.length - 1),
 			x
 		);
 	}
@@ -88,76 +112,91 @@ function CreateBarGroup(element: React.RefObject<SVGElement>, props: ChartStyle)
 		createYAxis(svg, yAxisGenerator, props);
 	}
 
-	const Bar = svg.append('g');
-	const createBar = Bar.selectAll('rect');
-	newData.forEach((d, i) => {
+	const Bar = svg.append("g");
+	const createBar = Bar.selectAll("rect");
+	const offset =
+		(groupedData.length * barWidth + (groupedData.length - 1) * barGap) / 2;
+	groupedData.forEach((d, i) => {
 		createBar
 			.data(d.value)
-			.join('rect')
-			.attr('class', 'bar_' + i)
-			.attr('fill', (d) => colorScale(d.key))
-			.attr('stroke', (d) => colorScale(d.key))
-			.attr('stroke-width', '1px')
-			.attr('fill-opacity', fill.opacity)
-			.attr('rx', fill.radius.x)
-			.attr('width', barWidth < 0 ? barWidth * -1 : barWidth)
-			.attr('height', (d) => Math.abs(yScale(0) - yScale(d.y)))
-			.attr('x', (d) => {
-				const x = xScale(d.x.toString());
-				return (x !== undefined ? x : 0) + xScale.bandwidth() / 6 + i * barWidth + i * barGap;
+			.join("rect")
+			.attr("class", "bar_" + i)
+			.attr("fill", (d) => colorScale(d.key))
+			.attr("stroke", (d) => colorScale(d.key))
+			.attr("stroke-width", "1px")
+			.attr("fill-opacity", fill.opacity)
+			.attr("rx", fill.radius.x)
+			.attr("width", barWidth < 0 ? barWidth * -1 : barWidth)
+			.attr("height", (d) => Math.abs(yScale(0) - yScale(d.y)))
+			.attr("x", (v) => {
+				const x = xScale(v.x.toString());
+				return (
+					(x !== undefined ? x : 0) +
+					xScale.bandwidth() / 2 +
+					i * barWidth +
+					i * barGap -
+					offset
+				);
 			})
-			.attr('y', (d) => yScale(d.y));
+			.attr("y", (d) => yScale(d.y));
 	});
 
 	createTitle(svg, props);
 
-	const barValue = svg.append('g');
-	const createBarValue = barValue.selectAll('text');
+	const barValue = svg.append("g");
+	const createBarValue = barValue.selectAll("text");
 
 	if (fill.value.enabled) {
-		newData.forEach((d, i) => {
+		groupedData.forEach((d, i) => {
 			createBarValue
 				.data(d.value)
-				.join('text')
+				.join("text")
 				.text((d) => d.y)
-				.attr('class', 'barValue_' + i)
-				.attr('font-size', font.size)
-				.attr('fill', fill.value.color)
-				.attr('font-weight', 600)
-				.attr('text-anchor', 'middle')
-				.attr('x', (d) => {
+				.attr("class", "barValue_" + i)
+				.attr("font-size", font.size)
+				.attr("fill", fill.value.color)
+				.attr("font-weight", 600)
+				.attr("text-anchor", "middle")
+				.attr("x", (d) => {
 					const x = xScale(d.x.toString());
-					return (x !== undefined ? x : 0) + xScale.bandwidth() / 6 + i * barWidth + i * barGap + barWidth / 2;
+					return (
+						(x !== undefined ? x : 0) +
+						xScale.bandwidth() / 2 +
+						i * barWidth +
+						i * barGap +
+						barWidth / 2 -
+						offset
+					);
 				})
-				.attr('y', (d) => yScale(d.y) + 15);
+				.attr("y", (d) => yScale(d.y) + 15);
 		});
 	}
 
 	if (animation.enabled) {
-		newData.forEach((d, i) => {
-			Bar.selectAll('.bar_' + i)
+		groupedData.forEach((d, i) => {
+			Bar.selectAll(".bar_" + i)
 				.data(d.value)
-				.attr('y', base.height - margin.bottom)
-				.attr('height', 0)
-				.attr('fill', 'rgba(0, 0, 0, 0)')
+				.attr("y", base.height - margin.bottom)
+				.attr("height", 0)
+				.attr("fill", "rgba(0, 0, 0, 0)")
 				.transition()
-				.attr('y', (d) => yScale(d.y))
-				.attr('height', (d) => Math.abs(yScale(0) - yScale(d.y)))
-				.attr('fill', (d) => colorScale(d.key))
+				.attr("y", (d) => yScale(d.y))
+				.attr("height", (d) => Math.abs(yScale(0) - yScale(d.y)))
+				.attr("fill", (d) => colorScale(d.key))
 				.duration(animation.duration)
 				.ease(easeExpInOut);
 
 			if (fill.value.enabled) {
 				barValue
-					.selectAll('.barValue_' + i)
+					.selectAll(".barValue_" + i)
 					.data(d.value)
-					.attr('y', base.height - margin.bottom)
+					.attr("y", base.height - margin.bottom)
 					.transition()
-					.attr('y', (d) => yScale(d.y) + 15)
+					.attr("y", (d) => yScale(d.y) + 15)
 					.textTween((d) => {
 						const f = d3.interpolate(0, d.y);
 						return (t) => {
-							return `${d3.format('.0f')(f(t))}`;
+							return `${d3.format(".0f")(f(t))}`;
 						};
 					})
 					.duration(animation.duration)
@@ -167,57 +206,57 @@ function CreateBarGroup(element: React.RefObject<SVGElement>, props: ChartStyle)
 	}
 
 	const onHover = (event: unknown, index: unknown) => {
-		newData.forEach((data, i) => {
+		groupedData.forEach((data, i) => {
 			if (i === index) {
-				Bar.selectAll('.bar_' + i)
+				Bar.selectAll(".bar_" + i)
 					.data(data.value)
 					.transition()
-					.attr('height', (d) => yScale(0) - yScale(d.y))
-					.attr('width', xScale.bandwidth() / 2)
-					.attr('x', (d) => {
+					.attr("height", (d) => yScale(0) - yScale(d.y))
+					.attr("width", xScale.bandwidth() / 2)
+					.attr("x", (d) => {
 						const x = xScale(d.x.toString());
 						return (x !== undefined ? x : 0) + xScale.bandwidth() / 4;
 					})
-					.attr('y', (d) => yScale(d.y))
-					.attr('fill', (d) => colorScale(d.key))
+					.attr("y", (d) => yScale(d.y))
+					.attr("fill", (d) => colorScale(d.key))
 					.duration(500)
 					.ease(easeExpOut);
 				barValue
-					.selectAll('.barValue_' + i)
+					.selectAll(".barValue_" + i)
 					.data(data.value)
 					.transition()
-					.attr('opacity', 1)
-					.attr('x', (d) => {
+					.attr("opacity", 1)
+					.attr("x", (d) => {
 						const x = xScale(d.x.toString());
 						return (x !== undefined ? x : 0) + xScale.bandwidth() / 2;
 					})
-					.attr('y', (d) => yScale(d.y) + 15)
+					.attr("y", (d) => yScale(d.y) + 15)
 					.duration(500)
 					.ease(easeExpOut);
 			} else {
-				Bar.selectAll('.bar_' + i)
+				Bar.selectAll(".bar_" + i)
 					.data(data.value)
 					.transition()
-					.attr('height', 0)
-					.attr('width', xScale.bandwidth() / 2)
-					.attr('x', (d) => {
+					.attr("height", 0)
+					.attr("width", xScale.bandwidth() / 2)
+					.attr("x", (d) => {
 						const x = xScale(d.x.toString());
 						return (x !== undefined ? x : 0) + xScale.bandwidth() / 4;
 					})
-					.attr('y', base.height - margin.bottom)
-					.attr('fill', 'rgba(0, 0, 0, 0)')
+					.attr("y", base.height - margin.bottom)
+					.attr("fill", "rgba(0, 0, 0, 0)")
 					.duration(500)
 					.ease(easeExpOut);
 				barValue
-					.selectAll('.barValue_' + i)
+					.selectAll(".barValue_" + i)
 					.data(data.value)
 					.transition()
-					.attr('opacity', 0)
-					.attr('x', (d) => {
+					.attr("opacity", 0)
+					.attr("x", (d) => {
 						const x = xScale(d.x.toString());
 						return (x !== undefined ? x : 0) + xScale.bandwidth() / 2;
 					})
-					.attr('y', yScale(0))
+					.attr("y", yScale(0))
 					.duration(500)
 					.ease(easeExpOut);
 			}
@@ -225,42 +264,58 @@ function CreateBarGroup(element: React.RefObject<SVGElement>, props: ChartStyle)
 	};
 
 	const noHover = () => {
-		newData.forEach((data, i) => {
-			Bar.selectAll('.bar_' + i)
+		groupedData.forEach((data, i) => {
+			Bar.selectAll(".bar_" + i)
 				.data(data.value)
 				.transition()
-				.attr('height', (d) => yScale(0) - yScale(d.y))
-				.attr('width', barWidth)
-				.attr('x', (d) => {
+				.attr("height", (d) => yScale(0) - yScale(d.y))
+				.attr("width", barWidth)
+				.attr("x", (d) => {
 					const x = xScale(d.x.toString());
-					return (x !== undefined ? x : 0) + xScale.bandwidth() / 6 + i * barWidth + i * barGap;
+					return (
+						(x !== undefined ? x : 0) +
+						xScale.bandwidth() / 2 +
+						i * barWidth +
+						i * barGap -
+						offset
+					);
 				})
-				.attr('y', (d) => yScale(d.y))
-				.attr('fill', (d) => colorScale(d.key))
+				.attr("y", (d) => yScale(d.y))
+				.attr("fill", (d) => colorScale(d.key))
 				.duration(500)
 				.ease(easeExpOut);
 			barValue
-				.selectAll('.barValue_' + i)
+				.selectAll(".barValue_" + i)
 				.data(data.value)
 				.transition()
-				.attr('opacity', 1)
-				.attr('x', (d) => {
+				.attr("opacity", 1)
+				.attr("x", (d) => {
 					const x = xScale(d.x.toString());
-					return (x !== undefined ? x : 0) + xScale.bandwidth() / 6 + i * barWidth + i * barGap + barWidth / 2;
+					return (
+						(x !== undefined ? x : 0) +
+						xScale.bandwidth() / 2 +
+						i * barWidth +
+						i * barGap +
+						barWidth / 2 -
+						offset
+					);
 				})
-				.attr('y', (d) => yScale(d.y) + 15)
+				.attr("y", (d) => yScale(d.y) + 15)
 				.duration(500)
 				.ease(easeExpOut);
 		});
 	};
 
 	if (legend.enabled) {
-		createLegend(svg, rowKeys, colorScale, props, onHover, noHover);
+		createLegend(svg, keys, colorScale, props, onHover, noHover);
 	}
 
 	if (tooltip.enabled) {
 		const { showTooltip, moveTooltip, hideTooltip } = createTooltip(svg, props);
-		Bar.selectAll('rect').on('mouseover', showTooltip).on('mousemove', moveTooltip).on('mouseleave', hideTooltip);
+		Bar.selectAll("rect")
+			.on("mouseover", showTooltip)
+			.on("mousemove", moveTooltip)
+			.on("mouseleave", hideTooltip);
 	}
 }
 
@@ -281,12 +336,12 @@ BarGroup.propTypes = {
 BarGroup.defaultProps = {
 	mapper: {
 		getX: (d: any) => d.x,
-		keys: ['y'],
+		getGroup: (d: any) => d.group,
 	},
 	base: {
 		width: undefined,
 		height: 300,
-		title: '',
+		title: "",
 		color: undefined,
 	},
 	tooltip: {
@@ -294,7 +349,7 @@ BarGroup.defaultProps = {
 		enabled: true,
 	},
 	xAxis: {
-		title: 'x',
+		title: "x",
 		type: d3.scaleTime,
 		domain: undefined,
 		range: undefined,
@@ -304,7 +359,7 @@ BarGroup.defaultProps = {
 		enabled: true,
 	},
 	yAxis: {
-		title: 'y',
+		title: "y",
 		type: d3.scaleLinear,
 		domain: undefined,
 		range: undefined,
@@ -324,14 +379,14 @@ BarGroup.defaultProps = {
 		enabled: true,
 	},
 	font: {
-		size: '12px',
+		size: "12px",
 	},
 	fill: {
 		opacity: 0.4,
 		value: {
 			// color: '#f2f0f0',
-			color: 'currentColor',
-			enabled: true,
+			color: "currentColor",
+			enabled: false,
 		},
 		radius: {
 			x: 3,
--- src/chart/circular/Doughnut.md
+++ src/chart/circular/Doughnut.md
@@ -1,9 +1,5 @@
 ```tsx
-let data = [
-  {'name': '臺北市', 'label': '高中以下', 'value': 4914143795516.0},
-  {'name': '臺北市', 'label': '大專', 'value': 7336112508338.0},
-  {'name': '臺北市', 'label': '研究所', 'value': 2006204037944.0}
-];
+let data = [{'name': 'month count', 'label': '8', 'value': 22}, {'name': 'month count', 'label': '6', 'value': 17}, {'name': 'month count', 'label': '12', 'value': 15}, {'name': 'month count', 'label': '9', 'value': 14}, {'name': 'month count', 'label': '11', 'value': 13}, {'name': 'month count', 'label': '10', 'value': 12}, {'name': 'month count', 'label': '7', 'value': 12}, {'name': 'month count', 'label': '1', 'value': 0}, {'name': 'month count', 'label': '2', 'value': 0}, {'name': 'month count', 'label': '3', 'value': 0}, {'name': 'month count', 'label': '4', 'value': 0}, {'name': 'month count', 'label': '5', 'value': 0}];
 
 <Doughnut
   data={data}
@@ -12,7 +8,7 @@ let data = [
     getY: (d) => d.value
   }}
   base={{
-    title: '臺北市',
+    title: 'Month Count',
     height: 500
   }}
 ></Doughnut>
